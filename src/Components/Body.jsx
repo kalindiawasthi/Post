7989b12161b2208d24827918833221e696d94151
Saurabh Indoria <saurabhindoria@Saurabhs-Macbook-Pro.local>
2021-12-06 19:18:32 +0530

    img load

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -6,11 +6,14 @@ function Body(){
     const [postList,setPostList]= useState([])
     const [loading,setLoading] = useState(false)
     const [scroll,setScroll] = useState(true)
-
-    function addPost (){
-        return(
-            null
-        )
+   
+    function changeStatus (event){
+       // console.log(event.target.id)
+        setPostList(postList.map((i) => {
+             return i.url === event.target.id?  {...i,ready: true} : {...i}
+            
+        }))
+        //console.log(postList)
     }
     useEffect(() => {
         if (scroll){
@@ -27,14 +30,18 @@ function Body(){
 
      useEffect(() => {
         setScroll(false)
-
+        
         Promise.all([fetch("https://api.thecatapi.com/v1/images/search?limit=5").then(response => response.json())
         ,fetch("https://catfact.ninja/facts?limit=5&max_length=140").then(response => response.json())])
         //fetch("https://dog.ceo/api/breeds/image/random/5")
         
         .then(data => { data[0].map((i,index) => {
-            return setPostList(mi => [...mi,{url : i.url,caption:data[1].data[index].fact}]) 
+            
+            return setPostList(mi => [...mi,{url : i.url,
+                caption : data[1].data[index].fact,
+                ready : false}]) 
         })
+      
             setScroll(true)
                 })
         //https://dog.ceo/api/breeds/image/random/5
@@ -54,7 +61,7 @@ function Body(){
 
     return(
         <div >
-                <Post list = {postList} onAdd = {addPost}  loading = {loading} />
+                <Post list = {postList}   loading = {loading}  onReady= {changeStatus}  />
                 
         </div>
         
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,26 +1,30 @@
 
 const Loading = require('react-loading-animation');
 
-function Post({list,loading}){
+function Post({list,loading,onReady}){
 
 
-
+  
     return(
        <div className="container-fluid">
         <div className="d-flex row justify-content-center ">
         {list.map((i,index) => {
+            
+           
              return(
                 
-            <div key = {index} className="card text-wrap  mb-4 mr-4 col-xs-12 col-md-3 border border-dark rounded shadow p-3 mb-5 bg-white rounded"  >   
+            <div key = {index} id = 'card'className="card col-xs-12 col-md-3"  >   
             
-            <img className="card-img-top img-fluid  "src={i.url} alt="Post" />
-            <div className="card-header text-muted text-wrap ">
-            {i.caption}
-            </div>
+            <img id={i.url} className="card-img-top img-fluid  "src={i.url} alt="Post"  onLoad={ (event) => {onReady(event)} }/>
+            {i.ready ? <div className="card-header text-muted text-wrap">{i.caption} </div> : 
+            <div className="card-header text-muted text-wrap">{i.ready} </div>}
+           
+            
             </div>
             
-            )
-        })}
+            )} 
+            
+        )}
        </div>
         {loading && <Loading />}
         </div>
