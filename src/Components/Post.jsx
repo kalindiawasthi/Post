5e28d41e22fbc9c7f24ef3fb2bf5ed7ed0fdd9c6
Saurabh Indoria <saurabhindoria@Saurabhs-Macbook-Pro.local>
2021-12-06 15:11:21 +0530

    display changes

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,22 +1,27 @@
+
 const Loading = require('react-loading-animation');
 
 function Post({list,loading}){
 
+
+
     return(
-       <div>
-        <div className="d-felx row justify-content-center">
+       <div className="container-fluid">
+        <div className="d-flex row justify-content-center ">
         {list.map((i,index) => {
-            return(
-            <div key = {index} className="card mb-4" style={{width: '70%'}} >   
-                <div className="card-body bg-light border-light">
-                    <span className="card-text"><b>Caption:</b>{i.caption}</span>
-                </div>
-                <img className="card-img-top img-fluid" src={i.url} alt="Post"/>
-            </div>)
+             return(
+                
+            <div key = {index} className="card text-wrap  mb-4 mr-4 col-xs-12 col-md-3 border border-dark rounded shadow p-3 mb-5 bg-white rounded"  >   
+            
+            <img className="card-img-top img-fluid  "src={i.url} alt="Post" />
+            <div className="card-header text-muted text-wrap ">
+            {i.caption}
+            </div>
+            </div>
+            
+            )
         })}
-       
-        
-        </div>
+       </div>
         {loading && <Loading />}
         </div>
         
